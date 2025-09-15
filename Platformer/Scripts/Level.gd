77f839f76f0d6bd285ab2bd2300f33fbe1f77743
Jonathan <jonathan@holstebroe.dk>
2025-09-15 20:27:58 +0200

    Remove files

--- Platformer/Scripts/Level.gd
+++ Platformer/Scripts/Level.gd
@@ -34,11 +34,12 @@ func remove_player(id: int):
 @rpc("any_peer", "call_local")
 func spawn_player_on_clients(id: int, role: int):
 	var player = player_scene.instantiate()
-	player.name = str(id)
+	player.name = str(role)
 	player.position = spawn_point.position
 	player.player_role = role
 	print("Player has role ", role)
 	add_child(player)
+	player.add_to_group("players")
 	player.set_multiplayer_authority(id)
 	if multiplayer.is_server():
 		Network.register_player(id, player)
